3755155b25d204669e82710e49ee760cf78260c0
concreteface <concreteface@users.noreply.github.com>
2016-03-20 14:34:06 -0400

    trying to deploy

---
+++ server.backup
@@ -0,0 +1,50 @@
+require "sinatra"
+require "pg"
+require_relative "./app/models/article"
+require 'pry'
+
+set :views, File.join(File.dirname(__FILE__), "app", "views")
+
+configure :development do
+  set :db_config, { dbname: "comics_finder_development" }
+end
+
+configure :test do
+  set :db_config, { dbname: "comics_finder_test" }
+end
+
+def db_connection
+  begin
+    connection = PG.connect(Sinatra::Application.db_config)
+    yield(connection)
+    ensure
+    connection.close
+  end
+end
+
+get '/' do
+  erb :home
+end
+
+get '/articles' do
+  @articles = db_connection {|conn| conn.exec("SELECT * FROM articles")}
+  erb :articles
+end
+
+get '/articles/new' do
+  @errors = []
+  erb :submit_article
+end
+
+post '/articles/new' do
+  article = Article.new({"title" => params[:title], "url" => params[:url], "description" => params[:description]})
+  @title = params[:title]
+  @url = params[:url]
+  @description = params[:description]
+  if article.valid?
+    article.save
+    redirect '/articles'
+  else @errors = article.errors
+    erb :submit_article
+  end
+end
--- server.rb
+++ server.rb
@@ -1,50 +1,71 @@
-require "sinatra"
-require "pg"
-require_relative "./app/models/article"
-require 'pry'
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'sinatra/reloader'
+require 'pg'
+require_relative 'app/lib/get_titles_urls'
+require_relative 'app/lib/validator'
+require_relative 'app/lib/order'
+include GetInfo
+include Validator
+include Order
 
 set :views, File.join(File.dirname(__FILE__), "app", "views")
 
 configure :development do
-  set :db_config, { dbname: "comics_finder_development" }
+  set :db_config, { dbname: "comics_development" }
 end
 
-configure :test do
-  set :db_config, { dbname: "comics_finder_test" }
+# configure :test do
+#   set :db_config, { dbname: "comics_finder_test" }
+# end
+
+configure :production do
+  uri = URI.parse(ENV["DATABASE_URL"])
+  set :db_config, {
+    host: uri.host,
+    port: uri.port,
+    dbname: uri.path.delete('/'),
+    user: uri.user,
+    password: uri.password
+  }
 end
 
 def db_connection
   begin
-    connection = PG.connect(Sinatra::Application.db_config)
+    connection = PG.connect(settings.db_config)
     yield(connection)
     ensure
     connection.close
   end
 end
 
+Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
+  require file
+  also_reload file
+end
+
 get '/' do
-  erb :home
+  redirect '/issues'
 end
 
-get '/articles' do
-  @articles = db_connection {|conn| conn.exec("SELECT * FROM articles")}
-  erb :articles
-end
-
-get '/articles/new' do
-  @errors = []
-  erb :submit_article
-end
-
-post '/articles/new' do
-  article = Article.new({"title" => params[:title], "url" => params[:url], "description" => params[:description]})
-  @title = params[:title]
-  @url = params[:url]
-  @description = params[:description]
-  if article.valid?
-    article.save
-    redirect '/articles'
-  else @errors = article.errors
-    erb :submit_article
+get '/issues' do
+  if validator(params[:date])
+    @message = ''
+    @date = Date.parse(params[:date])
+    get_titles_urls(@date)
+    if Issue.group(:release_date).count[@date] != @titles.length
+      @titles.each_with_index do |t, i|
+        Issue.create(title: t, image_url: @list.cover_url(@urls[i]), release_date: @date, writers: @list.writer(@urls[i]), artist: @list.artist(@urls[i]), description: @list.description(@urls[i]), publisher: @list.publisher(@urls[i]))
+      end
+    end
+    @message = "Here are the comics released on #{@date.month}/#{@date.day}/#{@date.year}."
+  else @message = "Please enter a valid date"
   end
+  @issues = Issue.where(release_date: @date).order(order(params))
+  erb :index
+end
+
+get '/issues/:id' do
+  @issue = Issue.find(params[:id])
+  erb :show
 end
